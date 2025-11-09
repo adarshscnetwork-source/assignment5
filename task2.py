9ffba8973a876b2ae66d7b836f535750761c128e
Adarsh Goswami <adarsh.scnetwork@gmail.com>
2025-11-10 01:24:28 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,9 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+print("Original list:", numbers)
+
+first_five = numbers[:5]
+print("Extracted first five elements:", first_five)
+
+reversed_list = first_five[::-1]
+print("Reversed extracted elements:", reversed_list)
